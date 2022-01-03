1eccbdb12a8bcf125576b96bf36aa99610013c7f
victorlee917 <victorlee917@gmail.com>
2022-01-03 14:08:58 +0900

    Updates

---
+++ precache-manifest.c9f30fdf26a23c7646547cb132c72c41.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db4c8259abe1d97b4c31bb4c87dabc11",
+    "url": "/portfolio_2020/index.html"
+  },
+  {
+    "revision": "8ebc7df361bcd665f225",
+    "url": "/portfolio_2020/static/js/2.e864ae18.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio_2020/static/js/2.e864ae18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5b8baea03517986fa41",
+    "url": "/portfolio_2020/static/js/main.2c2ed6f6.chunk.js"
+  },
+  {
+    "revision": "2611f996f36f2f5c06fb",
+    "url": "/portfolio_2020/static/js/runtime-main.2b1e910a.js"
+  },
+  {
+    "revision": "cba32d267ce7e8c378165814ecb3a05c",
+    "url": "/portfolio_2020/static/media/BrunchBG.cba32d26.png"
+  },
+  {
+    "revision": "31a77afddafbd1061236aaae6fd0cfbd",
+    "url": "/portfolio_2020/static/media/InstaBG.31a77afd.png"
+  },
+  {
+    "revision": "9d267a985b2d905971af72bb213304ce",
+    "url": "/portfolio_2020/static/media/Link.9d267a98.png"
+  },
+  {
+    "revision": "660c13861dffaf3b8bdbac1b40604594",
+    "url": "/portfolio_2020/static/media/Memoji.660c1386.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0f2479210d4f1f7297416896b72a07b5.js"
+  "/portfolio_2020/precache-manifest.c9f30fdf26a23c7646547cb132c72c41.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio_2020/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
